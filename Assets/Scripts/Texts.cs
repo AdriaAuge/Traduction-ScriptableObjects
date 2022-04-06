9e885044e49b3d97c24d0c6065be878ff89c9fa4
AdriaAuge <79134252+AdriaAuge@users.noreply.github.com>
2022-04-06 10:54:40 +0200

    Main

---
+++ Assets/Scripts/Texts.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Texts", fileName = "New Text")]
+public class Texts : ScriptableObject
+{
+    public string traductionLanguage;
+    public string traductionText;
+}
---
+++ Assets/Scripts/Traductions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Traductions : MonoBehaviour
+{
+    [Header("TRADUCTIONS")]
+    public Texts[] traductions;
+    public Text[] traductionLanguage;
+    public Text[] traductionText;
+}
